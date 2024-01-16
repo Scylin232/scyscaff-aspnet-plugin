8a95b92b48acf14d00098a1863bda1f111aebb95
Scylin232 <timyr3@ukr.net>
2024-01-16 13:50:30 +0100

    Service Generation Events Implementation; Automatic Solution Creation & Project Add; Fixed Update Command Handler Template;

--- AspNetFramework.cs
+++ AspNetFramework.cs
@@ -1,15 +1,52 @@
-﻿using System.Reflection;
+﻿using CliWrap;
+using System.Reflection;
 using ScyScaff.Core.Models.Plugins;
 
 namespace ScyScaffPlugin.AspNet;
 
-public class AspNetFramework : IFrameworkPlugin
+public class AspNetFramework : IFrameworkPlugin, IServiceGenerationEvents
 {
     public string FrameworkName => "aspnet";
     
     public string[] SupportedAuth { get; } = { "keycloak" };
     public string[] SupportedDatabases { get; } = { "postgresql" };
 
-    public string GetTemplateTreePath() =>
-        Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "TemplateTree\\");
+    public string GetTemplateTreePath() => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "TemplateTree\\");
+    
+    public async Task OnServiceGenerationEnded(DirectoryInfo serviceDirectory)
+    {
+        string[] projectsPath =
+        {
+            "API/API.csproj",
+            "Application/Application.csproj",
+            "Domain/Domain.csproj",
+            "Infrastructure/Infrastructure.csproj",
+            "SharedKernel/SharedKernel.csproj"
+        };
+        
+        await Cli.Wrap("dotnet")
+            .WithArguments(args => args
+                .Add("new")
+                .Add("sln")
+                .Add("--name")
+                .Add(serviceDirectory.Name))
+            .WithWorkingDirectory(serviceDirectory.FullName)
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteAsync();
+        
+
+        foreach (string projectPath in projectsPath)
+            await Cli.Wrap("dotnet")
+                .WithArguments(args => args
+                    .Add("sln")
+                    .Add($"{serviceDirectory.Name}.sln")
+                    .Add("add")
+                    .Add(projectPath))
+                .WithWorkingDirectory(serviceDirectory.FullName)
+                .WithValidation(CommandResultValidation.None)
+                .ExecuteAsync();
+    }
+    
+    // We don't need that:
+    public Task OnServiceGenerationStarted(DirectoryInfo serviceDirectory) => Task.CompletedTask;
 }
