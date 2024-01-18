86ff684d3b3f195ac3a404ed16d00b3ebb9589b7
Scylin232 <timyr3@ukr.net>
2024-01-18 19:20:40 +0100

    Docker Support;

--- AspNetFramework.cs
+++ AspNetFramework.cs
@@ -1,10 +1,12 @@
 ﻿using CliWrap;
 using System.Reflection;
 using ScyScaff.Core.Models.Plugins;
+using ScyScaff.Docker.Models.Plugins;
+using ScyScaff.Docker.Models.Builder;
 
 namespace ScyScaffPlugin.AspNet;
 
-public class AspNetFramework : IFrameworkPlugin, IServiceGenerationEvents
+public class AspNetFramework : IFrameworkPlugin, IServiceGenerationEvents, IDockerCompatible
 {
     public string FrameworkName => "aspnet";
     
@@ -44,6 +46,59 @@ public class AspNetFramework : IFrameworkPlugin, IServiceGenerationEvents
                 .WithWorkingDirectory(serviceDirectory.FullName)
                 .WithValidation(CommandResultValidation.None)
                 .ExecuteAsync();
+    }
+    
+    public IEnumerable<DockerComposeService> GetComposeServices(string projectName, string serviceName, int serviceIndex)
+    {
+        List<DockerComposeService> dockerComposeServices = new();
+        
+        DockerComposeService databaseService = new DockerComposeService
+        {
+            Image = "postgres:16.1",
+            ContainerName = $"{serviceName.ToLower()}-database",
+            EnvironmentVariables = new Dictionary<string, string>
+            {
+                { "POSTGRES_DB", $"{serviceName}Database" },
+                { "POSTGRES_USER", $"{serviceName}Login" },
+                { "POSTGRES_PASSWORD", $"{serviceName}Password" },
+                { "PGDATA", "/data/postgres" },
+                { "PGPORT", (5000 + serviceIndex).ToString() }
+            },
+            Volumes = new Dictionary<string, string>
+            {
+                { $"{serviceName.ToLower()}-database-volume", "/data/postgres" }
+            },
+            Ports = new Dictionary<int, int?>
+            {
+                { 5000 + serviceIndex, 5000 + serviceIndex }
+            },
+            ExtraProperties = "healthcheck:\n   test: [ \"CMD-SHELL\", \"pg_isready\" ]\n   interval: 10s\n timeout: 5s\n   retries: 5"
+        };
+        
+        DockerComposeService frameworkService = new DockerComposeService
+        {
+            Build = new ComposeBuild { Context = $"./{projectName}.{serviceName}/API", Dockerfile = "Dockerfile" }, 
+            ContainerName = $"{serviceName.ToLower()}-service",
+            Dependencies = new Dictionary<string, ComposeDependency>
+            {
+                { databaseService.ContainerName, new ComposeDependency { Condition = "service_healthy" } }
+            },
+            EnvironmentVariables = new Dictionary<string, string>
+            {
+                { "ASPNETCORE_ENVIRONMENT", "Development" },
+                { "ASPNETCORE_URLS", $"http://+:{5001 + serviceIndex}" },
+                { "CONNECTION_STRING_DEVELOPMENT", $"Host={databaseService.ContainerName};Database;Port=5000;Database={serviceName}Database;Username={serviceName}Login;Password={serviceName}Password;IncludeErrorDetail=true;" }
+            },
+            Ports = new Dictionary<int, int?>
+            {
+                { 5001 + serviceIndex, 5001 + serviceIndex }
+            }
+        };
+        
+        dockerComposeServices.Add(databaseService);
+        dockerComposeServices.Add(frameworkService);
+        
+        return dockerComposeServices;
     }
     
     // We don't need that:
