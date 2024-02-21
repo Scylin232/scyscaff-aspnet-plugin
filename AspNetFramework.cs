568e2aea243ea51770a66efd2833218f8978a82c
Scylin232 <timyr3@ukr.net>
2024-02-21 02:26:27 +0100

    Metrics Flag; Docker Compose Service Type Specifying; Ports in Variable;

--- AspNetFramework.cs
+++ AspNetFramework.cs
@@ -1,6 +1,7 @@
 ﻿using CliWrap;
 using System.Reflection;
 using ScyScaff.Core.Models.Plugins;
+using ScyScaff.Docker.Enums;
 using ScyScaff.Docker.Models.Plugins;
 using ScyScaff.Docker.Models.Builder;
 
@@ -12,6 +13,7 @@ public class AspNetFramework : IFrameworkTemplatePlugin, ITemplateGenerationEven
     
     public string[] SupportedAuth { get; } = { "auth0" };
     public string[] SupportedDatabases { get; } = { "postgresql" };
+    public string[] SupportedFlags { get; } = { "Metrics" };
 
     public string GetTemplateTreePath() => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "TemplateTree\\");
     
@@ -51,9 +53,13 @@ public class AspNetFramework : IFrameworkTemplatePlugin, ITemplateGenerationEven
     public IEnumerable<DockerComposeService> GetComposeServices(string projectName, string serviceName, int serviceIndex)
     {
         List<DockerComposeService> dockerComposeServices = new();
+
+        int databasePort = 5000 + serviceIndex;
+        int servicePort = 5001 + serviceIndex;
         
         DockerComposeService databaseService = new DockerComposeService
         {
+            Type = DockerComposeServiceType.Database,
             Image = "postgres:16.1",
             ContainerName = $"{serviceName.ToLower()}-database",
             EnvironmentVariables = new Dictionary<string, string>
@@ -62,7 +68,7 @@ public class AspNetFramework : IFrameworkTemplatePlugin, ITemplateGenerationEven
                 { "POSTGRES_USER", $"{serviceName}Login" },
                 { "POSTGRES_PASSWORD", $"{serviceName}Password" },
                 { "PGDATA", "/data/postgres" },
-                { "PGPORT", (5000 + serviceIndex).ToString() }
+                { "PGPORT", databasePort.ToString() }
             },
             Volumes = new Dictionary<string, string>
             {
@@ -70,13 +76,14 @@ public class AspNetFramework : IFrameworkTemplatePlugin, ITemplateGenerationEven
             },
             Ports = new Dictionary<int, int?>
             {
-                { 5000 + serviceIndex, 5000 + serviceIndex }
+                { databasePort, databasePort }
             },
             ExtraProperties = "healthcheck:\n   test: [ \"CMD-SHELL\", \"pg_isready\" ]\n   interval: 10s\n timeout: 5s\n   retries: 5"
         };
         
         DockerComposeService frameworkService = new DockerComposeService
         {
+            Type = DockerComposeServiceType.Framework,
             Build = new ComposeBuild { Context = $"./{projectName}.{serviceName}/API", Dockerfile = "Dockerfile" }, 
             ContainerName = $"{serviceName.ToLower()}-service",
             Dependencies = new Dictionary<string, ComposeDependency>
@@ -86,12 +93,12 @@ public class AspNetFramework : IFrameworkTemplatePlugin, ITemplateGenerationEven
             EnvironmentVariables = new Dictionary<string, string>
             {
                 { "ASPNETCORE_ENVIRONMENT", "Development" },
-                { "ASPNETCORE_URLS", $"http://+:{5001 + serviceIndex}" },
-                { "CONNECTION_STRING_DEVELOPMENT", $"Host={databaseService.ContainerName};Database;Port=5000;Database={serviceName}Database;Username={serviceName}Login;Password={serviceName}Password;IncludeErrorDetail=true;" }
+                { "ASPNETCORE_URLS", $"http://+:{servicePort}" },
+                { "CONNECTION_STRING_DEVELOPMENT", $"Host={databaseService.ContainerName};Database;Port={databasePort};Database={serviceName}Database;Username={serviceName}Login;Password={serviceName}Password;IncludeErrorDetail=true;" }
             },
             Ports = new Dictionary<int, int?>
             {
-                { 5001 + serviceIndex, 5001 + serviceIndex }
+                { servicePort, servicePort }
             }
         };
         
